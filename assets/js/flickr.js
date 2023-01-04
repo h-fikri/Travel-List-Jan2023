df3114fe9d9eb8db878331d513de083374e7fb3a
QuantumK <quantumk9@hotmail.com>
2023-01-04 20:55:20 +0000

    make call and get an image

--- assets/js/flickr.js
+++ assets/js/flickr.js
@@ -1,1 +1,34 @@
-console.log("connected");
+// console.log("connected");
+var KEY = "af8b14f5d63c1d15f94a974b1e37ffb7";
+var SECRET = "d88fb2bc3f7b2a07";
+
+function getFlickrImage(cityName) {
+  var API_KEY = KEY;
+  var URL = `https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${API_KEY}&text=${cityName}&per_page=4&format=json&nojsoncallback=1`;
+
+  $.ajax({
+    type: "GET",
+    url: URL,
+  }).then(function (response) {
+    var photoArray = response.photos.photo;
+    console.log(response);
+    console.log(response.photos);
+    console.log(response.photos.photo[0].id);
+    console.log(response.photos.photo[0].secret);
+    console.log(response.photos.photo[0].server);
+
+    var firstPhoto = photoArray[0];
+    var imageDetails = {
+      id: firstPhoto.id,
+      secret: firstPhoto.secret,
+      server: firstPhoto.server,
+    };
+    var queryURL = `https://live.staticflickr.com/${imageDetails.server}/${imageDetails.id}_${imageDetails.secret}.jpg`;
+    $("#image").attr("src", queryURL);
+  });
+}
+
+getFlickrImage("London");
+// https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=af8b14f5d63c1d15f94a974b1e37ffb7&text=london&per_page=4&format=json&nojsoncallback=1
+
+// https://live.staticflickr.com/{server-id}/{id}_{secret}_{size-suffix}.jpg
