a8f5bd6c63ad797a7bb36ed46cf7b3f478be60b5
QuantumK <quantumk9@hotmail.com>
2023-01-05 19:21:14 +0000

    get 4 images from unsplash based on input and display them in target element

--- assets/js/unsplash.js
+++ assets/js/unsplash.js
@@ -4,41 +4,42 @@
 // -------------------------------
 
 var API_KEY = "6KNhdCaJ8Ct-wL5wW3cjzWPIC-Os2V-DTAJxL1-u2HA";
-var SECRET = "IB9Isji_B6sbWfFns8-N_4eeOnn4NLBAK4BjZKZlqEc"; // not in use
-var targetId = "images";
+var targetId = "images"; // change the ID of the html element where the images will be appended to (don't include the # in the name)
 
 // function called with a cityName parameter
 function getUnsplashImages(cityName) {
   // query url
-  // var URL = `https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${API_KEY}&text=${cityName}&per_page=4&format=json&nojsoncallback=1`;
-  var URL = `https://api.unsplash.com/photos/?client_id=${API_KEY}&query=${cityName}`;
-  console.log(cityName);
+  var URL = `https://api.unsplash.com/search/photos/?client_id=${API_KEY}&query=${cityName}&per_page=4`;
+
   // AJAX call
   $.ajax({
     type: "GET",
     url: URL,
   }).then(function (response) {
-    // console.log(response[0].urls.regular);
-    //     var photoArray = response.photos.photo;
-    var imagesLinks = [];
-    response.map(function (el) {
+    var dataArray = response.results;
+    // array that will have 4 objects - one for each image - with url and alt info of each
+    var imagesInfo = [];
+    dataArray.map(function (el) {
       var imageUrl = el.urls.regular;
-      imagesLinks.push(imageUrl);
+      var imageAlt = el.alt_description;
+      var imageInfo = {
+        url: imageUrl,
+        alt: imageAlt,
+      };
+      imagesInfo.push(imageInfo);
     });
-
-    displayImages(imagesLinks);
-    //   });
-    // }
+    // calling function to display images in html
+    displayImages(imagesInfo);
   });
 }
 
-// function that displays images by creating img element in images div
-function displayImages(imagesLinksArray) {
-  imagesLinksArray.map(function (link) {
-    imgEl = $("<img>").attr("src", link);
+// function that displays images by creating img element in #targetId div (with src and alt attrubutes filled in)
+function displayImages(imagesInfoArray) {
+  imagesInfoArray.map(function (image) {
+    imgEl = $("<img>").attr("src", image.url);
+    imgEl.attr("alt", image.alt);
     $(`#${targetId}`).append(imgEl);
   });
 }
-
 // testing example
-getUnsplashImages("London");
+// getUnsplashImages("London");
