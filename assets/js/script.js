e283ae6679a989b33fc621e28fa1fac946dc7d5a
QuantumK <quantumk9@hotmail.com>
2023-01-06 16:11:03 +0000

    change form button type to submit and take user's input and store it

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,3 +6,11 @@ $("#random-btn").on("click", function () {
   console.log(randomCity);
   getUnsplashImages(randomCity);
 });
+
+// form event listener
+$("#search-btn").on("click", function (event) {
+  event.preventDefault();
+  var cityInput = $("#search-box").val().trim();
+  console.log(cityInput);
+});
+// getting user input when form is submitted
