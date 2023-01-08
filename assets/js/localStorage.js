e6786c508751b261c65ace3b92ad11909ed5b625
QuantumK <quantumk9@hotmail.com>
2023-01-08 16:31:31 +0000

    add city to cities function

--- assets/js/localStorage.js
+++ assets/js/localStorage.js
@@ -4,3 +4,19 @@ var cities = [];
 function addToLocalStorage(cities) {
   localStorage.setItem("cities", JSON.stringify(cities));
 }
+
+// function that adds city cities state
+function addCityToCities(city) {
+  let cityExists = false;
+  cities.map((ct) => {
+    if (ct.cityName === city) {
+      console.log(`city already exists`);
+      cityExists = true;
+      return;
+    }
+  });
+  if (!cityExists) {
+    console.log("It doesnt exist so I add it!");
+    cities.push(city);
+  }
+}
