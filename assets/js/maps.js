cf4a8e0e6337c5dc388b5fee8a8440543998fd70
QuantumK <quantumk9@hotmail.com>
2023-01-11 22:28:12 +0000

    modal display for invalid input

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 // Name of the location
 let locationName;
@@ -7,82 +7,83 @@ let _status;
 // Object to store the lat and lng
 let coordinates;
 
-const key = 'AIzaSyCxxgDfu2qDm0BdsQyrxeBQnPOtJWJO1RU';
+const key = "AIzaSyCxxgDfu2qDm0BdsQyrxeBQnPOtJWJO1RU";
 
 function createMap(city) {
-	// # GEOCODING API
-	const geoLocationURL = `https://maps.googleapis.com/maps/api/geocode/json?address=${city}&key=${key}`;
+  // # GEOCODING API
+  const geoLocationURL = `https://maps.googleapis.com/maps/api/geocode/json?address=${city}&key=${key}`;
 
-	$.ajax({
-		type: 'GET',
-		url: geoLocationURL,
-		success: function (response) {
-			_status = response.status;
+  $.ajax({
+    type: "GET",
+    url: geoLocationURL,
+    success: function (response) {
+      _status = response.status;
 
-			switch (_status) {
-				case 'OK':
-					locationName = response.results[0].formatted_address;
-					coordinates = response.results[0].geometry.location;
-					localStorage.setItem(
-						'location',
-						JSON.stringify(coordinates)
-					);
-					break;
-				case 'ZERO_RESULTS':
-					alert('Sorry, your search did not match any data');
-					break;
-				case 'OVER_DAILY_LIMIT':
-					alert(
-						'Please, check your API key is not missing or invalid'
-					);
-					break;
-				case 'OVER_QUERY_LIMIT':
-					alert('The API key set is over its query quota');
-					break;
-				case 'REQUEST_DENIED':
-					alert('Request denied');
-					break;
-				case 'INVALID_REQUEST':
-					alert('Please, check the query is not missing');
-					break;
-				case 'UNKNOWN_ERROR':
-					alert('Server Error');
-					break;
+      switch (_status) {
+        case "OK":
+          locationName = response.results[0].formatted_address;
+          coordinates = response.results[0].geometry.location;
+          localStorage.setItem("location", JSON.stringify(coordinates));
+          break;
+        case "ZERO_RESULTS":
+          // alert("Sorry, your search did not match any data");
+          $("#search-modal").modal("show");
+          break;
+        case "OVER_DAILY_LIMIT":
+          // alert("Please, check your API key is not missing or invalid");
+          $("#search-modal").modal("show");
+          break;
+        case "OVER_QUERY_LIMIT":
+          // alert("The API key set is over its query quota");
+          $("#search-modal").modal("show");
+          break;
+        case "REQUEST_DENIED":
+          // alert("Request denied");
+          $("#search-modal").modal("show");
+          break;
+        case "INVALID_REQUEST":
+          // alert("Please, check the query is not missing");
+          $("#search-modal").modal("show");
+          break;
+        case "UNKNOWN_ERROR":
+          // alert("Server Error");
+          $("#search-modal").modal("show");
+          break;
 
-				default:
-					break;
-			}
-		},
+        default:
+          break;
+      }
+    },
 
-		// # MAPS API
-	}).then(() => {
-		const location = new google.maps.LatLng(coordinates);
+    // # MAPS API
+  }).then(() => {
+    const location = new google.maps.LatLng(coordinates);
 
-		let mapProperties = {
-			center: location,
-			zoom: 4,
-			mapTypeId: 'terrain',
-			disableDefaultUI: true,
-			scrollwheel: false,
-		};
+    let mapProperties = {
+      center: location,
+      zoom: 4,
+      mapTypeId: "terrain",
+      disableDefaultUI: true,
+      scrollwheel: false,
+    };
 
-		// Creates the map with the properties passed
-		let map = new google.maps.Map(
-			document.querySelector('#google-map'),
-			mapProperties
-		);
+    // Creates the map with the properties passed
+    let map = new google.maps.Map(
+      document.querySelector("#google-map"),
+      mapProperties
+    );
 
-		// * FEATURES FOR THE MAP
-		// The marker, positioned at given coordinates
-		const marker = new google.maps.Marker({
-			position: location,
-			map: map,
-		});
-		// Zoom to 8 when clicking on marker
-		google.maps.event.addListener(marker, 'click', function () {
-			var pos = map.getZoom();
-			map.setZoom(8);
-			map.setCenter(marker.getPosition());
-		});
-	});
+    // * FEATURES FOR THE MAP
+    // The marker, positioned at given coordinates
+    const marker = new google.maps.Marker({
+      position: location,
+      map: map,
+    });
+    // Zoom to 8 when clicking on marker
+    google.maps.event.addListener(marker, "click", function () {
+      var pos = map.getZoom();
+      map.setZoom(8);
+      map.setCenter(marker.getPosition());
+    });
+  });
 }
